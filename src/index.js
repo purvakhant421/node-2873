ef20c6e8e194e89f3be9c7e5786cf94edef9e893
purva <purvaratanapara48@gmail.com>
2023-08-10 13:08:53 +0530

    newadd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const { connectDB } = require("./db/dbconnection");
+const routes = require("./routes/v1");
 
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
   fs.readFile('demo.html',function(err,data){
     res.writeHead(200,{'Content-Type': 'text/html'});
-    res.write(data); 
-  res.end(); 
-  });
-  }).listen(8080);
+    res.write(data); res.end(); });
+  }).listen(3000);
 
 // express js
 // const express = require("express")
 // const app = express()
 // app.listen(8080)
+
+/** upload image */
+// app.use(express.static(`./public`));
+
+app.use("/v1", routes);
 
 /** Database connection */
 connectDB()
